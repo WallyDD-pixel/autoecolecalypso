5ceb4fd7d0e1c0c7dd38dd8b4a9240307907d0df
WallyDD-pixel <wallydibombepro@gmail.com>
2022-10-23 13:31:38 +0200

    new version

---
+++ public/pages/contact-us.php
@@ -0,0 +1,390 @@
+<!--
+=========================================================
+* Material Kit 2 - v3.0.4
+=========================================================
+
+* Product Page:  https://www.creative-tim.com/product/material-kit 
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+* Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. -->
+<!DOCTYPE html>
+<html lang="en" itemscope itemtype="http://schema.org/WebPage">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+  <title>
+  CALYPSO AUTO ÉCOLE
+
+  </title>
+  <!--     Fonts and icons     -->
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
+  <!-- Nucleo Icons -->
+  <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- Font Awesome Icons -->
+  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  <!-- Material Icons -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp:opsz,wght,FILL,GRAD@48,400,0,0" />
+  <!-- CSS Files -->
+  <link id="pagestyle" href="../assets/css/material-kit.css?v=3.0.4" rel="stylesheet" />
+</head>
+
+<body class="contact-us">
+  <!-- Navbar -->
+  <!-- Navbar -->
+<div class="container position-sticky z-index-sticky top-0"><div class="row"><div class="col-12">
+  <nav class="navbar navbar-expand-lg  blur border-radius-xl top-0 z-index-fixed shadow position-absolute my-3 py-2 start-0 end-0 mx-4">
+    <div class="container-fluid px-0">
+      <a class="navbar-brand font-weight-bolder ms-sm-3" href="/index.html" rel="tooltip" title="Designed and Coded by Wall-E" data-placement="bottom" target="_blank">
+        CALYPSO AUTO ÉCOLE
+      </a>
+      <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon mt-2">
+          <span class="navbar-toggler-bar bar1"></span>
+          <span class="navbar-toggler-bar bar2"></span>
+          <span class="navbar-toggler-bar bar3"></span>
+        </span>
+      </button>
+      <div class="collapse navbar-collapse pt-3 pb-2 py-lg-0 w-100" id="navigation">
+        <ul class="navbar-nav navbar-nav-hover ms-auto">
+          <li class="nav-item dropdown dropdown-hover mx-2">
+            <a class="nav-link ps-2 d-flex cursor-pointer align-items-center" id="dropdownMenuPages" data-bs-toggle="dropdown" aria-expanded="false">
+              
+              <span class="material-symbols-outlined">
+                school
+                </span>
+                 Formation
+              <img src="./assets/img/down-arrow-dark.svg" alt="down-arrow" class="arrow ms-auto ms-md-2">
+            </a>
+            <div class="dropdown-menu dropdown-menu-animation ms-n3 dropdown-md p-3 border-radius-xl mt-0 mt-lg-3" aria-labelledby="dropdownMenuPages">
+              <div class="d-none d-lg-block">
+                <h6 class="dropdown-header text-dark font-weight-bolder d-flex align-items-center px-1">
+                  Nos formations
+                </h6>
+    <a href="./pages/about-us.html" class="dropdown-item border-radius-md">
+      <span>Code de la route</span>
+    </a>
+    <a href="./pages/contact-us.php" class="dropdown-item border-radius-md">
+      <span>Conduite automobile</span>
+    </a>
+    
+  </div>
+  
+  <div class="d-lg-none">
+    
+    <h6 class="dropdown-header text-dark font-weight-bolder d-flex align-items-center px-1">
+      Nos formations
+    </h6>
+    <a href="./pages/about-us.html" class="dropdown-item border-radius-md">
+      <span>Code de la route</span>
+    </a>
+    <a href="./pages/contact-us.php" class="dropdown-item border-radius-md">
+      <span>Conduite automobile</span>
+    </a>
+    
+  </div>
+  
+            </div>
+          </li>
+  
+          <li class="nav-item dropdown dropdown-hover mx-2">
+            <a class="nav-link ps-2 d-flex cursor-pointer align-items-center" id="dropdownMenuBlocks" data-bs-toggle="dropdown" aria-expanded="false">
+              <span class="material-symbols-sharp">
+                done_outline
+                </span>
+              Résultats
+              <img src="./assets/img/down-arrow-dark.svg" alt="down-arrow" class="arrow ms-auto ms-md-2">
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end dropdown-menu-animation dropdown-md dropdown-md-responsive p-3 border-radius-lg mt-0 mt-lg-3" aria-labelledby="dropdownMenuBlocks">
+              <div class="d-none d-lg-block">
+    <li class="nav-item dropdown dropdown-hover dropdown-subitem">
+      <a class="dropdown-item py-2 ps-3 border-radius-md" href="https://www.securite-routiere.gouv.fr/resultats-du-permis-de-conduire#/step-connexion">
+        <div class="w-100 d-flex align-items-center justify-content-between">
+          <div>
+            <h6 class="dropdown-header text-dark font-weight-bolder d-flex justify-content-cente align-items-center p-0">Consultez vos résultats</h6>
+            <span class="text-sm">Accéder à vos résultats à l'épreuve du permis de conduire</span>
+          </div>
+          
+        </div>
+      </a>
+      
+    </li>
+  
+    
+  
+    
+  
+    
+  
+    
+  </div>
+  
+  <div class="row d-lg-none">
+    <div class="col-md-12">
+      <div class="d-flex mb-2">
+        <div class="icon h-10 me-3 d-flex mt-1">
+          <i class="ni ni-single-copy-04 text-gradient text-primary"></i>
+        </div>
+        <div class="w-100 d-flex align-items-center justify-content-between">
+          <div>
+            <h6 class="dropdown-header text-dark font-weight-bolder d-flex justify-content-cente align-items-center p-0">consultez vos résultats</h6>
+          </div>
+        </div>
+      </div>
+      <a class="dropdown-item ps-3 border-radius-md mb-1" href="https://www.securite-routiere.gouv.fr/resultats-du-permis-de-conduire#/step-connexion">
+        Accéder à vos résultats à l'épreuve du permis de conduire
+      </a>
+
+    </div>
+  </div>
+  
+            </ul>
+          </li>
+  
+          <li class="nav-item dropdown dropdown-hover mx-2">
+            <a class="nav-link ps-2 d-flex cursor-pointer align-items-center" id="dropdownMenuDocs" data-bs-toggle="dropdown" aria-expanded="false">
+              <span class="material-symbols-outlined">
+                call
+                </span>
+              Contact
+              <img src="./assets/img/down-arrow-dark.svg" alt="down-arrow" class="arrow ms-auto ms-md-2">
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end dropdown-menu-animation dropdown-md dropdown-md-responsive mt-0 mt-lg-3 p-3 border-radius-lg" aria-labelledby="dropdownMenuDocs">
+              <div class="d-none d-lg-block">
+    <ul class="list-group">
+      <li class="nav-item list-group-item border-0 p-0">
+        <a class="dropdown-item py-2 ps-3 border-radius-md" href=" ./pages/contact-us.php">
+          <h6 class="dropdown-header text-dark font-weight-bolder d-flex justify-content-cente align-items-center p-0">Où nous trouver</h6>
+          <span class="text-sm">poser nous une question nous y répondrons dans les meilleurs délais</span>
+        </a>
+      </li>
+    </ul>
+  </div>
+  
+  <div class="row d-lg-none">
+    <div class="col-md-12 g-0">
+      <a class="dropdown-item py-2 ps-3 border-radius-md" href="./pages/contact-us.php">
+        <h6 class="dropdown-header text-dark font-weight-bolder d-flex justify-content-cente align-items-center p-0">Où nous trouver</h6>
+        <span class="text-sm">poser nous une question nous y répondrons dans les meilleurs délais</span>
+      </a>
+    </div>
+  </div>
+  
+            </ul>
+          </li>
+          
+          <li class="nav-item my-auto ms-3 ms-lg-0">
+            
+            <a  href = "tel:+0130304295" class="btn btn-sm  bg-gradient-primary  mb-0 me-1 mt-2 mt-md-0">01.30.30.42.95</a>
+            
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+        <!-- End Navbar -->
+      </div>
+    </div>
+  </div>
+  <!-- -------- START HEADER 8 w/ card over right bg image ------- -->
+  <section>
+    <div class="page-header min-vh-100">
+      <div class="container">
+        <div class="row">
+          <div class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 start-0 text-center justify-content-center flex-column">
+            <div class="position-relative h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center" style="background-image: url('../assets/img/illustrations/illustration-signin.jpg'); background-size: cover;" loading="lazy"></div>
+          </div>
+          <div class="col-xl-5 col-lg-6 col-md-7 d-flex flex-column ms-auto me-auto ms-lg-auto me-lg-5">
+            <div class="card d-flex blur justify-content-center shadow-lg my-sm-0 my-sm-6 mt-8 mb-5">
+              <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2 bg-transparent">
+                <div class="bg-gradient-primary shadow-primary border-radius-lg p-3">
+                  <h3 class="text-white text-primary mb-0">Contactez-nous </h3>
+                </div>
+              </div>
+              <div class="card-body">
+                <p class="pb-3">
+                Pour plus de renseignement veuillez nous contacter au 01.30.30.42.95 et en nous laisser un message et on vous donnera une réponse pertinente dans les plus brefs délais.
+                </p>
+                <form id="contact.php" method="post" autocomplete="off">
+                  <div class="card-body p-0 my-3">
+                    <div class="row">
+                      <div class="col-md-6">
+                        <div class="input-group input-group-static mb-4">
+                          <label>Nom Prenom</label>
+                          <input type="email" class="form-control" placeholder="Nom prenom">
+                        </div>
+                      </div>
+                      <div class="col-md-6 ps-md-2">
+                        <div class="input-group input-group-static mb-4">
+                          <label>Email</label>
+                          <input type="email" class="form-control" placeholder="albertdupont@gmail.com">
+                        </div>
+                      </div>
+                    </div>
+                    <div class="form-group mb-0 mt-md-0 mt-4">
+                      <div class="input-group input-group-static mb-4">
+                        <label>Comment pouvons-nous vous aider?</label>
+                        <textarea name="message" class="form-control" id="message" rows="6" placeholder="Décrivez votre problème en au moins 250 caractères"></textarea>
+                      </div>
+                    </div>
+                    <div class="row">
+                      <div class="col-md-12 text-center">
+                        <button type="submit" class="btn bg-gradient-primary mt-3 mb-0">Envoyer le message</button>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- -------- END HEADER 8 w/ card over right bg image ------- -->
+  <footer class="footer pt-5 mt-5">
+  <div class="container">
+    <div class=" row">
+      <div class="col-md-3 mb-4 ms-auto">
+        <div>
+          <a href="https://www.creative-tim.com/product/material-kit">
+            <img src="./assets/img/logo-ct-dark.png" class="mb-3 footer-logo" alt="main_logo">
+          </a>
+          <h6 class="font-weight-bolder mb-4">Auto école Calypso</h6>
+        </div>
+        <div>
+          <ul class="d-flex flex-row ms-n3 nav">
+            <li class="nav-item">
+              <a class="nav-link pe-1" href="https://www.facebook.com/CreativeTim" target="_blank">
+                <i class="fab fa-facebook text-lg opacity-8"></i>
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link pe-1" href="https://twitter.com/creativetim" target="_blank">
+                <i class="fab fa-twitter text-lg opacity-8"></i>
+              </a>
+            </li>
+
+
+            <li class="nav-item">
+              <a class="nav-link pe-1" href="https://www.youtube.com/channel/UCVyTG4sCw-rOvB9oHkzZD1w" target="_blank">
+                <i class="fab fa-youtube text-lg opacity-8"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+
+
+      <div class="col-md-2 col-sm-6 col-6 mb-4">
+        <div>
+          <h6 class="text-sm">Auto école</h6>
+          <ul class="flex-column ms-n3 nav">
+            <li class="nav-item">
+              <a class="nav-link" href="./pages/contact-us.php" target="_blank">
+                Contact
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="https://www.securite-routiere.gouv.fr/resultats-du-permis-de-conduire#/step-connexion" target="_blank">
+                résultats permis
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="https://www.creative-tim.com/blog" target="_blank">
+                Nos formations
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="col-md-2 col-sm-6 col-6 mb-4">
+        <div>
+          <h6 class="text-sm">Formations</h6>
+          <ul class="flex-column ms-n3 nav">
+            <li class="nav-item">
+              <a class="nav-link" href="https://iradesign.io/" target="_blank">
+                Permis B - manuel
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="https://www.creative-tim.com/bits" target="_blank">
+                Permis B - Automatique
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="https://www.creative-tim.com/affiliates/new" target="_blank">
+                Permis AAC
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="col-md-2 col-sm-6 col-6 mb-4">
+        <div>
+          <h6 class="text-sm">Besoin d'aide</h6>
+          <ul class="flex-column ms-n3 nav">
+            <li class="nav-item">
+              <a class="nav-link" href="./pages/contact-us.php" target="_blank">
+                Nous contacter
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="./pages/contact-us.php" target="_blank">
+                En savoir plus
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="./pages/contact-us.php" target="_blank">
+                Localisation
+              </a>
+            </li>
+
+            
+
+          </ul>
+        </div>
+      </div>
+
+      
+
+      <div class="col-12">
+        <div class="text-center">
+          <p class="text-dark my-4 text-sm font-weight-normal">
+            Tous les droits sont réservés. Droits d'auteur © <script>document.write(new Date().getFullYear())</script> Auto école Calypso <a href="https://github.com/WallyDD-pixel" target="_blank">by Wall-E</a>.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+  <!--   Core JS Files   -->
+  <script src="../assets/js/core/popper.min.js" type="text/javascript"></script>
+  <script src="../assets/js/core/bootstrap.min.js" type="text/javascript"></script>
+  <script src="../assets/js/plugins/perfect-scrollbar.min.js"></script>
+  <!--  Plugin for Parallax, full documentation here: https://github.com/wagerfield/parallax  -->
+  <script src="../assets/js/plugins/parallax.min.js"></script>
+  <!-- Control Center for Material UI Kit: parallax effects, scripts for the example pages etc -->
+  <!--  Google Maps Plugin    -->
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDTTfWur0PDbZWPr7Pmq8K3jiDp0_xUziI"></script>
+  <script src="../assets/js/material-kit.min.js?v=3.0.4" type="text/javascript"></script>
+</body>
+
+</html>
